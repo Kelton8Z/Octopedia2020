b7d1ba7c503361a06c2ac501e759e6092788bde3
yajiewang0822 <yajiew@andrew.cmu.edu>
2020-07-28 21:46:43 -0400

    try page relocation

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/login', function (req, res, next) {
-    res.sendFile('/home/site/wwwroot/views/login.html');
+    res.render('../views/login.html');
 });
 
 module.exports = router;
